ad69d35719e07b61cbaa4380b27811fa2c194361
Daniel Klitgaard <42070753+DanielKragh@users.noreply.github.com>
2019-05-22 13:24:34 +0200

    footer done

--- js/main.js
+++ js/main.js
@@ -1,15 +1,25 @@
-$(function(){
+$(function () {
     // navbar border animation
-    var hr = $(".hr-ani");
-    var active = $(".active");
-    var position = active.position();
-    var top = position.top;
-    var left = position.left;
+    navAni();
 
-    hr.css({top: top+active.height(), left:left+active.width()/2-hr.width()/2, display:"block"});
+    $(window).on("resize", function () {
+        navAni();
+    });
 
     // video
     var vid = document.getElementById("video");
     vid.playbackRate = 0.5;
- 
+
 });
+function navAni() {
+        var hr = $(".hr-ani");
+        var active = $(".active");
+        var position = active.position();
+        var top = position.top;
+        var left = position.left;
+        hr.css({
+            top: top + active.height(),
+            left: left + active.width() / 2 - hr.width() / 2,
+            display: "block"
+        });
+    }
