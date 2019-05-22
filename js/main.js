d48f10121f738ef78c7e977892998fa81cc40b1e
Daniel Klitgaard <42070753+DanielKragh@users.noreply.github.com>
2019-05-22 13:09:22 +0200

    video

--- js/main.js
+++ js/main.js
@@ -8,6 +8,8 @@ $(function(){
 
     hr.css({top: top+active.height(), left:left+active.width()/2-hr.width()/2, display:"block"});
 
-    
-    
+    // video
+    var vid = document.getElementById("video");
+    vid.playbackRate = 0.5;
+ 
 });
