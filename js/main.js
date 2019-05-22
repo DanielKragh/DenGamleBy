b52d7011950fa5d8deff0d06cec9475133b4e1ec
Daniel Klitgaard <42070753+DanielKragh@users.noreply.github.com>
2019-05-22 11:14:52 +0200

    header update

--- js/main.js
+++ js/main.js
@@ -1,4 +1,13 @@
 $(function(){
     // navbar border animation
+    var hr = $(".hr-ani");
+    var active = $(".active");
+    var position = active.position();
+    var top = position.top;
+    var left = position.left;
 
+    hr.css({top: top+active.height(), left:left+active.width()/2-hr.width()/2});
+
+    
+    
 });
