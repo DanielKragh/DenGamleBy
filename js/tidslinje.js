51241bda18f1c31d08a6e250e3b20289b2a02796
oliverslott <oliverslottsony@gmail.com>
2019-06-03 09:09:43 +0200

    Made tidslinje script perfect.

--- js/tidslinje.js
+++ js/tidslinje.js
@@ -1,51 +1,58 @@
 //https://scrollmagic.io/docs/ScrollMagic.Scene.html
-$(function () {
+var addIndicators = false;
+
+//We use onload here instead because jquery's onload event happens to early.
+window.onload = function(){
     var controller = new ScrollMagic.Controller();
-    //console.log(controller);
-    var FirstDotDuration = $("#firstDot").offset().top - $(window).scrollTop() - $(window).height() / 2;
-    var SecondDotDuration = $("#secondDot").offset().top - $(window).scrollTop() - $(window).height() / 2;
-    var ThirdDotDuration = $("#thirdDot").offset().top - $(window).scrollTop() - $(window).height() / 2;
-    console.log(SecondDotDuration);
+    var FirstDotDuration = $("#firstDot").position().top - $(window).height() / 2;
+    var SecondDotDuration = $("#secondDot").position().top - $(window).height() / 2;
+    var ThirdDotDuration = $("#thirdDot").position().top - $(window).height() / 2;
     var tween = new TimelineMax()
         .add(TweenMax.to($("#linje path"), 0.8, { strokeDashoffset: 2229, ease: Linear.easeNone }))
-        .add(TweenMax.from($("#firstDot"), 0.2, {opacity: 0}));
     var scene = new ScrollMagic.Scene({duration: FirstDotDuration })
         .setTween(tween)
-        .addIndicators()
         .addTo(controller)
     var tween2 = new TimelineMax()
-        .add(TweenMax.to($("#linje path"), 0.9, { strokeDashoffset: 1487, ease: Linear.easeNone }))
-        .add(TweenMax.from($("#secondDot"), 0.1, {opacity: 0}))
-    var scene2 = new ScrollMagic.Scene({ triggerElement: "#firstDot", duration: SecondDotDuration})
+        .add(TweenMax.to($("#linje path"), 0.9, { strokeDashoffset: 1487, ease: Linear.easeNone }));
+    var scene2 = new ScrollMagic.Scene({ triggerElement: "#firstDot", duration: SecondDotDuration - FirstDotDuration})
         .setTween(tween2)
-        .addIndicators()
         .addTo(controller);
     var tween3 = new TimelineMax()
-        .add(TweenMax.to($("#linje path"), 0.9, { strokeDashoffset: 637, ease: Linear.easeNone }))
-        .add(TweenMax.from($("#thirdDot"), 0.1, {opacity: 0}));
-    var scene3 = new ScrollMagic.Scene({ triggerElement: "#secondDot", duration: ThirdDotDuration})
+        .add(TweenMax.to($("#linje path"), 0.9, { strokeDashoffset: 637, ease: Linear.easeNone }));
+    var scene3 = new ScrollMagic.Scene({ triggerElement: "#secondDot", duration: ThirdDotDuration - SecondDotDuration})
         .setTween(tween3)
-        .addIndicators()
         .addTo(controller);
     scene.on("end", function(){
-        scene2.duration($("#secondDot").offset().top - $(window).scrollTop() - $(window).height() / 2);
         $(".left").eq(0).toggleClass("fade-in-from-left");
         $(".right").eq(0).toggleClass("fade-in-from-right");
+        $("#firstDot").toggleClass("test");
     });
     scene2.on("end", function(){
-        scene3.duration($("#thirdDot").offset().top - $(window).scrollTop() - $(window).height() / 2);
         $(".left").eq(1).toggleClass("fade-in-from-left");
         $(".right").eq(1).toggleClass("fade-in-from-right");
+        $("#secondDot").toggleClass("test");
     });
     scene3.on("end", function(){
         $(".left").eq(2).toggleClass("fade-in-from-left");
         $(".right").eq(2).toggleClass("fade-in-from-right");
+        $("#thirdDot").toggleClass("test");
     });
-    $(window).on("scroll", function () {
-        //console.log($(window).scrollTop());
-        //console.log($("#firstDot").offset().top/2 - $(window).scrollTop());
+
+    if(addIndicators){
+        scene.addIndicators();
+        scene2.addIndicators();
+        scene3.addIndicators();
+    }
+    
+    $(window).on("resize", function(){
+        FirstDotDuration = $("#firstDot").position().top - $(window).height() / 2;
+        SecondDotDuration = $("#secondDot").position().top - $(window).height() / 2;
+        ThirdDotDuration = $("#thirdDot").position().top - $(window).height() / 2;
+        scene.duration(FirstDotDuration);
+        scene2.duration(SecondDotDuration - FirstDotDuration);
+        scene3.duration(ThirdDotDuration - SecondDotDuration);
     });
-});
+}
 //FirstDot: 2229
 //SecondDot: 1487
 //ThirdDot: 637
